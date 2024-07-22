a5285c36af105c19606fb9801b17992ccd053224
Dario Reis <guttyerresjunior1995@gmail.com>
2024-07-22 05:38:18 -0300

    Feat: defining rule for our page component

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -7,9 +7,14 @@ export interface PaginaProps {
 
 export default function Pagina(props: PaginaProps) {
   return (
-    <div className="flex flex-col">
-      <Cabecalho/>
-      <main>{props.children}</main>
+    <div className="flex flex-col min-h-screen">
+      <Cabecalho />
+      <main className={`
+        flex-1 w-[1200px] mx-auto
+        ${props.className ?? ''} py-10
+        `}>
+        {props.children}
+      </main>
     </div>
   );
 }
