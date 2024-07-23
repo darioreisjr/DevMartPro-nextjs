4433a58e09103e3f73d149516b461c9625ddf122
Dario Reis <guttyerresjunior1995@gmail.com>
2024-07-23 20:18:53 -0300

    Feat: implementing add-to-cart logic

--- src/data/contexts/ContextoCarrinho.tsx
+++ src/data/contexts/ContextoCarrinho.tsx
@@ -1,16 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import ItemCarrinho from "../model/ItemCarrinho";
+import Produto from "../model/Produto";
 
 interface ContextoCarrinhoProps {
-  numero: number;
+  itens: ItemCarrinho[];
+  qtdeDeItens: number;
+  adicionar: (item: Produto) => void;
 }
 
 const ContextoCarrinho = createContext<ContextoCarrinhoProps>({} as any);
 
 export function ProvedorCarrinho(props: any) {
+  const [itens, setIntens] = useState<ItemCarrinho[]>([]);
+
+  function adicionar(produto: Produto) {
+    const indice = itens.findIndex((i) => i.produto.id === produto.id);
+
+    if (indice === -1) {
+      setIntens([...itens, { produto, quantidade: 1 }]);
+    } else {
+      const novosItens = [...itens];
+      novosItens[indice].quantidade++;
+      setIntens(novosItens);
+    }
+  }
+
   return (
     <ContextoCarrinho.Provider
       value={{
-        numero: 1000,
+        itens,
+        adicionar,
+        get qtdeDeItens() {
+          return itens.reduce((total, item) => total + item.quantidade, 0);
+        }
       }}
     >
       {props.children}
