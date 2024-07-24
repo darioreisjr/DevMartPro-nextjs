b6d074b912c78b45ea59af7917396502bef3c7c9
Dario Reis <guttyerresjunior1995@gmail.com>
2024-07-24 05:09:32 -0300

    Feat: implementing part of the component totaling purchase and finalizing

--- src/app/(loja)/carrinho/page.tsx
+++ src/app/(loja)/carrinho/page.tsx
@@ -1,26 +1,30 @@
 "use client";
 import AreaItemCarrinho from "@/components/carrinho/AreaItemCarrinho";
 import CarrinhoVazio from "@/components/carrinho/carrinhoVazio";
+import TotalCarrinho from "@/components/carrinho/TotalCarrinho";
 import Pagina from "@/components/template/Pagina";
 import useCarrinho from "@/data/hooks/useCarrinho";
 
 export default function PaginaCarrinho() {
   const { itens, adicionar, remover } = useCarrinho();
   return (
-    <Pagina>
+    <Pagina className="flex flex-col gap-10">
       {itens.length === 0 ? (
         <CarrinhoVazio />
       ) : (
-        <div className="flex flex-col gap-5">
-          {itens.map((item) => (
-            <AreaItemCarrinho
-              key={item.produto.id}
-              item={item}
-              adicionar={(item) => adicionar(item.produto)}
-              remover={(item) => remover(item.produto)}
-            />
-          ))}
-        </div>
+        <>
+          <div className="flex flex-col gap-5">
+            {itens.map((item) => (
+              <AreaItemCarrinho
+                key={item.produto.id}
+                item={item}
+                adicionar={(item) => adicionar(item.produto)}
+                remover={(item) => remover(item.produto)}
+              />
+            ))}
+          </div>
+          <TotalCarrinho itens={itens} />
+        </>
       )}
     </Pagina>
   );
