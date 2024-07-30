2f258a7bb6b53d4903c4a708c8a8806e336da604
Dario Reis <guttyerresjunior1995@gmail.com>
2024-07-30 06:09:58 -0300

    Feat: add favicon to project as per issue #2. Closes #2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:['images.unsplash.com', 'plus.unsplash.com'],
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' }],
   }
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,14 @@ export const metadata: Metadata = {
   description: "O marketplace definitivo para desenvolvedores. Encontre as melhores ferramentas, recursos, e produtos para otimizar seu desenvolvimento de software. Qualidade, inovação e suporte especializado ao seu alcance.",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
