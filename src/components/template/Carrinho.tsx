7859b3759490637d7a69586b172587ef6b5f2f50
Dario Reis <guttyerresjunior1995@gmail.com>
2024-07-31 20:58:24 -0300

    fix(cart): resolve main responsiveness issue in cart area

--- src/components/template/Carrinho.tsx
+++ src/components/template/Carrinho.tsx
@@ -8,7 +8,7 @@ export default function Carrinho() {
     <Link href="/carrinho">
       <div className="flex relative">
         <IconShoppingCart size={32} stroke={1} />
-        <div className="absolute -top-2.5 -right-2.5 w-6 h-6 bg-red-600 rounded-full flex justify-center items-center text-xs">
+        <div className="absolute -top-2.5 -right-2.5 w-6 h-6 bg-red-500 rounded-full flex justify-center items-center text-xs">
           {qtdeDeItens}
         </div>
       </div>
